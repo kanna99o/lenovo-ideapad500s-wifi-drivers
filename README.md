90c7fea86b03588cf403d5e1519cbc03a84b5187
Rory Bradford <roryrjb@gmail.com>
2016-04-23 16:05:03 +0100

    initial

---
+++ README.md
@@ -0,0 +1,12 @@
+# What is this?
+
+A very small script that _should_ enable the Broadcom Wi-Fi chipset on Linux, this is specifically for Debian-based distributions. I have successfully tested this on Ubuntu 16.04 and is known to work on Debian 8.0, YMMV.
+
+This is 100% copied from this __Stack Overflow__ question: http://unix.stackexchange.com/questions/260538/i-cant-connect-to-wi-fi-no-wlan0-device-on-iwconfig, specifically [__Dinir's__](http://unix.stackexchange.com/users/155206/dinir) answer.
+
+### Requires
+
+* `git`
+* `wget`
+* `build-essential`
+
---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd /tmp
+git clone https://github.com/kvalo/ath10k-firmware.git
+cd ath10k-firmware/QCA9377/hw1.0
+sudo mkdir -p /lib/firmware/ath10k/QCA9377/hw1.0
+sudo cp board.bin /lib/firmware/ath10k/QCA9377/hw1.0
+sudo cp firmware-5.bin_WLAN.TF.1.0-00267-1 /lib/firmware/ath10k/QCA9377/hw1.0/firmware-5.bin
+sudo modprobe -r ath10k_pci
+cd /tmp
+wget https://www.kernel.org/pub/linux/kernel/projects/backports/2015/11/20/backports-20151120.tar.gz
+tar -xf backports-20151120.tar.gz
+cd backports-20151120
+make defconfig-ath10
+make
+sudo make install
+
